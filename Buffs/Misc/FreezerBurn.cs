77450390efcaa99297a0be22eaa1efaa4945a479
Mistermavin96 <mars2earth@goowy.com>
2015-03-05 19:29:45 -0500

    Bananas
    Burnurnurs

---
+++ Buffs/Misc/FreezerBurn.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Terraria;
+using TAPI;
+
+namespace LRPMod.Buffs.Misc
+{
+    public class FreezerBurn : TAPI.ModBuff
+    {
+        
+        public override void Effects(Player player, int index)
+		{
+			 player.AddBuff (39, 0, true);
+			 player.AddBuff (44, 0, true);
+		}
+	}
+}
